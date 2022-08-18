68e3f1f8a04e3f8d13b04d878c60511d4f80e401
swf <swf@mail.ustc.edu.cn>
2022-08-18 14:31:16 +0800

    test GPU

---
+++ sftp.py
@@ -0,0 +1,19 @@
+import paramiko
+import os
+
+client = paramiko.SSHClient()  # 获取SSHClient实例
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect("192.168.31.53", username="bb", password="bb")  # 连接SSH服务端
+transport = client.get_transport()  # 获取Transport实例
+ 
+# 创建sftp对象，SFTPClient是定义怎么传输文件、怎么交互文件
+sftp = paramiko.SFTPClient.from_transport(transport)
+ 
+# 将本地 api.py 上传至服务器 /www/test.py。文件上传并重命名为test.py
+sftp.put("/home/ustc-swf/cuda-test.py", os.path.join("/home/bb", 'cuda-test.py'))
+ 
+# 将服务器 /www/test.py 下载到本地 aaa.py。文件下载并重命名为aaa.py
+# sftp.get("/www/test.py", "E:/aaa.py")
+ 
+# 关闭连接
+client.close()
